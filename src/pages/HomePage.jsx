3af549d6741be0acbc726dd25e29733f55b88816
José Inacio <inaciojosea13@gmail.com>
2024-11-19 19:23:37 +0100

    done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import StudentCard from "../components/StudentCard";
-
+import { Link } from "react-router-dom";
 import studentsData from "../assets/students.json";
 
 function HomePage() {
@@ -17,11 +17,12 @@ function HomePage() {
           <span style={{ flexBasis: "20%" }}>Email</span>
           <span style={{ flexBasis: "20%" }}>Phone</span>
         </div>
-
+        <Link to=" /students/:studentId"></Link>
       {students &&
         students.map((student) => {
           return (
               <StudentCard key={student._id} {...student} />
+              
           );
         })}
     </div>
